7a1bb417e07ff162bcb7df07534b69d9733a7265
tyspt <jiaweitang828@gmail.com>
2020-11-14 20:41:19 +0100

    add page refreshing when exiting scan QR mode

--- lib/main.dart
+++ lib/main.dart
@@ -74,11 +74,10 @@ class _MyHomePageState extends State<MyHomePage> {
       barcodeScanRes = '';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted || barcodeScanRes.isEmpty || barcodeScanRes == '-1') {
+      // Exit the handover mode and do a page refresh if next scan is cancelled
       _currentHandoverUUID = null;
+      _loadPackages();
       return;
     }
 

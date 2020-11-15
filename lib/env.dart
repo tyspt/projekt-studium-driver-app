607037265ac5c030600bf9b5d84269442a3d21f8
tyspt <jiaweitang828@gmail.com>
2020-11-15 11:37:10 +0100

    add step to archive build artifact in repo

--- lib/env.dart
+++ lib/env.dart
@@ -2,6 +2,6 @@ const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
 const testConfig = {'baseUrl': 'http://192.168.2.115:8080'};
 
-const productionConfig = {'baseUrl': 'https://...'};
+const productionConfig = {'baseUrl': 'https://jiaweitang.com/api'};
 
 final environment = isProduction ? productionConfig : testConfig;
